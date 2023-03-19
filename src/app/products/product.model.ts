cf021ce8fea46b6b94eee55fc83c12aa731a2781
martscastrillo <mart.castrillo@gmail.com>
2023-03-20 00:04:29 +0100

    add CRUD

--- src/app/products/product.model.ts
+++ src/app/products/product.model.ts
@@ -4,7 +4,13 @@ import { BaseModel} from './../base.model'
 export type Sizes = 'S' | 'M' | 'L' | 'XL';
 export interface Product extends BaseModel{
     title: string;
+    image: string;
+    description: string;
     stock: number;
     size?: Sizes;
+    color: string;
+    price: string;
     category : Category;
+    isNew : boolean;
+    tags: string[];
 }
